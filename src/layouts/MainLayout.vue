0a2881fc95e9f03ca85239c924018f84c7f4f094
jasurxamrayev <jasurxamraoyev@gmail.com>
2022-05-03 18:11:11 +0845

    dfsdf

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+
+
+  </q-layout>
+</template>
+
+
